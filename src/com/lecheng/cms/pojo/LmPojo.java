1f7ae86febf773843875c98951ef5af2f2834f57
pv199109mnm <415206438@qq.com>
2012-08-21 11:30:21 +0800

    栏目组

---
+++ src/com/lecheng/cms/pojo/LmPojo.java
@@ -0,0 +1,18 @@
+package com.lecheng.cms.pojo;
+
+public class LmPojo {
+private int id;
+private String lmname;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getLmname() {
+	return lmname;
+}
+public void setLmname(String lmname) {
+	this.lmname = lmname;
+}
+}
