c8f133ebed2ddaa7510f55bfba3e2d417e4e50fb
WangXingJuan123 <1542165692@qq.com>
2012-08-22 04:26:14 +0000

    后台

---
+++ WebRoot/chenggong.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" import="java.util.*" pageEncoding="UTF-8"%>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+  <head>
+    <title>My JSP 'chenggong.jsp' starting page</title>
+  </head>
+  
+  <body>
+  <!--
+  if(session.getAttribute("name")==null){
+			response.sendRedirect("index.jsp");	
+		} 
+   %> 
+   -->
+    <h2>修改成功！</h2>
+    <a href="xiugai.jsp">返回</a>
+  </body>
+</html>
---
+++ src/com/lecheng/cms/servlet/XiugaimimaServlet.java
@@ -0,0 +1,96 @@
+package com.lecheng.cms.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.lecheng.cms.dao.UserDao;
+
+public class XiugaimimaServlet extends HttpServlet {
+
+	/**
+	 * Constructor of the object.
+	 */
+	public XiugaimimaServlet() {
+		super();
+	}
+
+	/**
+	 * Destruction of the servlet. <br>
+	 */
+	public void destroy() {
+		super.destroy(); // Just puts "destroy" string in log
+		// Put your code here
+	}
+
+	/**
+	 * The doGet method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to get.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out
+				.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">");
+		out.println("<HTML>");
+		out.println("  <HEAD><TITLE>A Servlet</TITLE></HEAD>");
+		out.println("  <BODY>");
+		out.print("    This is ");
+		out.print(this.getClass());
+		out.println(", using the GET method");
+		out.println("  </BODY>");
+		out.println("</HTML>");
+		out.flush();
+		out.close();
+	}
+
+	/**
+	 * The doPost method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to post.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		request.setCharacterEncoding("UTF-8");
+		//
+		String oldpasword = request.getParameter("oldpasword");
+		String newpasword = request.getParameter("newpasword");
+		//String newname2 = request.getParameter("newpasword2");
+		boolean flag = false;
+		UserDao ud = new UserDao();
+		flag = ud.xiugai(newpasword, oldpasword);
+		if(flag){
+			response.sendRedirect("../chenggong.jsp"); //重定向
+		}else{
+			response.sendRedirect("../shibai.jsp");
+		}
+	}
+
+	/**
+	 * Initialization of the servlet. <br>
+	 *
+	 * @throws ServletException if an error occurs
+	 */
+	public void init() throws ServletException {
+		// Put your code here
+	}
+
+}
