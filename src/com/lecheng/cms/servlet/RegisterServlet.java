10bfa722da595bbc14e6a532140209c89282a84f
WangXingJuan123 <1542165692@qq.com>
2012-08-23 11:24:17 +0000

    登录终结

---
+++ src/com/lecheng/cms/servlet/RegisterServlet.java
@@ -0,0 +1,84 @@
+package com.lecheng.cms.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.lecheng.cms.dao.UserDao;
+
+public class RegisterServlet extends HttpServlet {
+
+	/**
+	 * Constructor of the object.
+	 */
+	public RegisterServlet() {
+		super();
+	}
+
+	/**
+	 * Destruction of the servlet. <br>
+	 */
+	public void destroy() {
+		super.destroy(); // Just puts "destroy" string in log
+		// Put your code here
+	}
+
+	/**
+	 * The doGet method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to get.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		this.doPost(request, response);
+	}
+
+	/**
+	 * The doPost method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to post.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		//获取name值
+		String usname = request.getParameter("yhm");
+		//获取真实姓名
+		String name = request.getParameter("yhxm");
+		//密码
+		String pasword = request.getParameter("mm");
+		boolean flag = false;
+		UserDao ud = new UserDao();
+		flag = ud.reg(usname,pasword,name);
+		if(flag){
+			response.sendRedirect("../chenggong.jsp"); //重定向
+		}else{
+			response.sendRedirect("../shibai.jsp");
+		}
+	}
+
+	/**
+	 * Initialization of the servlet. <br>
+	 *
+	 * @throws ServletException if an error occurs
+	 */
+	public void init() throws ServletException {
+		// Put your code here
+	}
+
+}
